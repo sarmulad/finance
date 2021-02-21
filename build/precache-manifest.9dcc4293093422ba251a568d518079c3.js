42467ffe4c219cdd96b26d8bde95ab8d61e7d7c0
josh <oriakhi78@gmail.com>
2021-02-21 20:10:51 +0100

    I think I am done

---
+++ build/precache-manifest.9dcc4293093422ba251a568d518079c3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "245ee8c8d3ee8fb130fd5814efcbad81",
+    "url": "/index.html"
+  },
+  {
+    "revision": "42f04260b2e79d038a87",
+    "url": "/static/css/main.031998ee.chunk.css"
+  },
+  {
+    "revision": "5def14834e70bd487a8b",
+    "url": "/static/js/2.965696c1.chunk.js"
+  },
+  {
+    "revision": "fd13b75043dc3712e2809bca390c0314",
+    "url": "/static/js/2.965696c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42f04260b2e79d038a87",
+    "url": "/static/js/main.f14cfaeb.chunk.js"
+  },
+  {
+    "revision": "257c4ecbd575bd70aa97",
+    "url": "/static/js/runtime-main.40076d06.js"
+  },
+  {
+    "revision": "29606476d181ae12c5555859c48efc05",
+    "url": "/static/media/financial-advisor-bkgd.29606476.jpg"
+  }
+]);
