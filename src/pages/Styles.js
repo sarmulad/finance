c31e156e6d4ea0f5186ee5b151dc16134866c1b2
josh <oriakhi78@gmail.com>
2021-02-23 15:17:57 +0100

    STashed commit before pull

--- src/pages/Styles.js
+++ src/pages/Styles.js
@@ -2,10 +2,11 @@ import { makeStyles } from '@material-ui/core/styles';
 
 export default makeStyles(()=>({
   background:{
-    // background: 'url("../images/background.jpg")',
-    // backgroundSize:'cover',
-    // minHeight:'100vh',
+    background: 'url("../images/background.jpg")',
+    backgroundSize:'cover',
+    minHeight:'100vh',
     // width:'100vw',
+    padding: '5%'
     
   }
 }))
